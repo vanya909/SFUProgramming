1d126a70b0cbbffde618bb84baea1d5e2ab4a9db
Ivan <vanya909x75@gmail.com>
2021-12-19 17:39:29 +0700

    add: 10 lab

---
+++ TenthApp/TenthApp/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace TenthApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime start = DateTime.Now;
+            School school = new School();
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                school.students.Add(new Student()
+                {
+                    Number = i,
+                    CountPhone = i % 21,
+                    CountLunch = 5,
+                    Position = (Position)(i % 3)
+                }) ;
+            }
+
+            school.Reward();
+            DateTime end = DateTime.Now;
+            Console.WriteLine((end - start).TotalSeconds);
+        }
+    }
+
+
+    struct Student : IComparable
+    {
+        public int Number { get; set; }
+        public int CountPhone { get; set; }
+        public int CountLunch { get; set; }
+
+        public Position Position { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+            if (obj is Student)
+            {
+                Student student = (Student)obj;
+                int difference = this.CountPhone - student.CountPhone;
+
+                return difference == 0 ? this.Position.CompareTo(student.Position) : difference;
+            }
+            else throw new ArgumentException("Must be Student!");
+        }
+    }
+
+
+    //class Student : IComparable
+    //{
+    //    public int Number { get; set; }
+    //    public int CountPhone { get; set; }
+    //    public int CountLunch { get; set; }
+
+    //    public Position Position { get; set; }
+
+    //    public int CompareTo(object obj)
+    //    {
+    //        if (obj == null) return 1;
+    //        if (obj is Student)
+    //        {
+    //            Student student = (Student)obj;
+    //            int difference = this.CountPhone - student.CountPhone;
+
+    //            return difference == 0 ? this.Position.CompareTo(student.Position) : difference;
+    //        }
+    //        else throw new ArgumentException("Must be Student!");
+    //    }
+    //}
+
+
+    enum Position
+    {
+        HIGH,
+        JUNIOR,
+        PRE
+    }
+}
---
+++ TenthApp/TenthApp/School.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TenthApp
+{
+    class School
+    {
+        public List<Student> students { get; set; } = new List<Student>();
+
+        public (Student, Student) FindMinMaxEmployee()
+        {
+            students.Sort();
+            return (students[students.Count - 1], students[0]);
+        }
+
+        public void Reward()
+        {
+            Student goodStudent = FindMinMaxEmployee().Item1;
+            Student badStudent = FindMinMaxEmployee().Item2;
+
+            goodStudent.CountLunch += badStudent.CountLunch;
+            badStudent.CountLunch = 0;
+        }
+    }
+}
